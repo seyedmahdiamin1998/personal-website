2500c503926c2967a209474f35dd05806aeb3825
seyedmahdiamin <seyedmahdiamin1998@gmail.com>
2022-07-14 14:06:43 +0430

    Complete

--- assets/Bitcoin/js/raceLine.js
+++ assets/Bitcoin/js/raceLine.js
@@ -30,7 +30,7 @@ var ETHUSDTdata = function () {
 var ADAUSDTdata = function () {
     var tmp = null;
     $.ajax({
-        url: "https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=15m",
+        url: "https://api.binance.com/api/v3/klines?symbol=ADAUSDT&interval=15m",
         type: 'GET',
         dataType: 'json', // added data type
         async: false,
@@ -55,46 +55,56 @@ var SHIBUSDTdata = function () {
     return tmp;
 }();
 
+
 ////////////////////////////////////////////////////////////////////////
-var dataapache = function () {
-    var tmp = null;
-    $.ajax({
-        url: "https://echarts.apache.org/examples/data/asset/data/life-expectancy-table.json",
-        type: 'GET',
-        dataType: 'json', // added data type
-        async: false,
-        success: function (data) {
-            tmp = data;
-        }
-    });
-    return tmp;
-}();
+let dataapache =[['Date','Close','Symbol']]
+let date3
+let timestampRaceLine
+for (let i = 1; i < BTCUSDTdata.length; i++) {
+    timestampRaceLine = BTCUSDTdata[i][0]
+    date3 = new Date(timestampRaceLine);
+    date4 = date3.getUTCFullYear()+"-"+(date3.getUTCMonth()+1)+"-"+date3.getUTCDate()+" "+date3.getUTCHours()+":"+date3.getUTCMinutes() ;
+    dataapache.push([date4, Math.round(((BTCUSDTdata[i][4]-BTCUSDTdata[0][4])*100/BTCUSDTdata[i][4])*100)/100, 'BTCUSDT']) 
+    dataapache.push([date4, Math.round(((ETHUSDTdata[i][4]-ETHUSDTdata[0][4])*100/ETHUSDTdata[i][4])*100)/100, 'ETHUSDT']) 
+    dataapache.push([date4, Math.round(((ADAUSDTdata[i][4]-ADAUSDTdata[0][4])*100/ADAUSDTdata[i][4])*100)/100, 'ADAUSDT']) 
+    dataapache.push([date4,Math.round(((SHIBUSDTdata[i][4]-SHIBUSDTdata[0][4])*100/SHIBUSDTdata[i][4])*100)/100,'SHIBUSDT']) 
+}
+
+
+// dataapache = BTCUSDTdata.concat(ETHUSDTdata)
+// dataapache = dataapache.concat(ADAUSDTdata)
+// dataapache = dataapache.concat(ADAUSDTdata)
+// dataapache = dataapache.concat(SHIBUSDTdata)
+
 console.log(dataapache)
 //////////////////////////////////////////////////////////////////////////////
 
 var chartDom = document.getElementById('CompareBTCUSDT');
 var myChart = echarts.init(chartDom);
 var option;
-
-run(dataapache);
+let plotted=0
+$(window).scroll(function() {
+    if ($('#CompareBTCUSDT').is(':visible')) {
+        if (plotted==0){
+            run(dataapache);
+            plotted=1
+        }
+    }
+});
 
 
 function run(_rawData) {
-    // var countries = ['Australia', 'Canada', 'China', 'Cuba', 'Finland', 'France', 'Germany', 'Iceland', 'India', 'Japan', 'North Korea', 'South Korea', 'New Zealand', 'Norway', 'Poland', 'Russia', 'Turkey', 'United Kingdom', 'United States'];
-    const countries = [
-        'Finland',
-        'France',
-        'Germany',
-        'Iceland',
-        'Norway',
-        'Poland',
-        'Russia',
-        'United Kingdom'
+    const symbols = [
+        'BTCUSDT',
+        'ETHUSDT',
+        'ADAUSDT',
+        'SHIBUSDT'
     ];
     const datasetWithFilters = [];
     const seriesList = [];
-    echarts.util.each(countries, function (country) {
-        var datasetId = 'dataset_' + country;
+    echarts.util.each(symbols, function (symbol) {
+        
+        var datasetId = 'dataset_' + symbol;
         datasetWithFilters.push({
             id: datasetId,
             fromDatasetId: 'dataset_raw',
@@ -102,21 +112,21 @@ function run(_rawData) {
                 type: 'filter',
                 config: {
                     and: [
-                        { dimension: 'Year', gte: 1930 },
-                        { dimension: 'Country', '=': country }
+                        { dimension: 'Symbol', '=': symbol }
                     ]
                 }
             }
         });
+        
         seriesList.push({
             type: 'line',
             datasetId: datasetId,
             showSymbol: false,
-            name: country,
+            name: symbol,
             endLabel: {
                 show: true,
                 formatter: function (params) {
-                    return params.value[3] + ': ' + params.value[0];
+                    return params.value[2] + ': ' + params.value[1];
                 }
             },
             labelLayout: {
@@ -126,16 +136,29 @@ function run(_rawData) {
                 focus: 'series'
             },
             encode: {
-                x: 'Year',
-                y: 'Income',
-                label: ['Country', 'Income'],
-                itemName: 'Year',
-                tooltip: ['Income']
+                x: 'Date',
+                y: 'Close',
+                label: ['Symbol', 'Close'],
+                itemName: 'Date',
+                tooltip: ['Close']
             }
         });
     });
     
     option = {
+        toolbox: {
+            feature: {
+              dataZoom: {
+                yAxisIndex: 'none'
+              },
+              restore: {},
+              saveAsImage: {}
+            }
+        },
+        legend:{
+            bottom: 10,
+            left: 'center',
+        },
         animationDuration: 10000,
         dataset: [
             {
@@ -145,7 +168,7 @@ function run(_rawData) {
             ...datasetWithFilters
         ],
         title: {
-            text: 'Income of Germany and France since 1950'
+            text: 'Percentage change between the current and a prior Close values'
         },
         tooltip: {
             order: 'valueDesc',
@@ -156,7 +179,7 @@ function run(_rawData) {
             nameLocation: 'middle'
         },
         yAxis: {
-            name: 'Income'
+            name: 'Percentage change '
         },
         grid: {
             right: 140
